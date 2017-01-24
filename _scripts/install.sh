c20f5bc3a7bcbe3fb174f1029afaa1eb7d029268
Nurmukhamed Artykaly <nurmukh@nbc.kz>
2017-01-24 11:56:57 +0600

    Removed debug, and tree command

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-set -x
-
-# Let's see what files inside travis home
-tree
 
 # Encrypt archive and extract archive
 openssl aes-256-cbc -K $encrypted_26b4962af0e7_key -iv $encrypted_26b4962af0e7_iv -in encryptedfiles.tar.enc -out encryptedfiles.tar -d
