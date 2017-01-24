a16c91da8f60a34297291cad489a0ec3f4055e3f
Nurmukhamed Artykaly <nurmukh@nbc.kz>
2017-01-24 11:07:07 +0600

    Updated for archives
    Script was updated for new logic:
    * encrypt archive
    * untar archive
    * rm archive
    * move ssh key to safe place
    * chown Rakefile, _config.yml

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -4,20 +4,22 @@ set -x
 # Let's see what files inside travis home
 tree
 
+# Encrypt archive and extract archive
+openssl aes-256-cbc -K $encrypted_26b4962af0e7_key -iv $encrypted_26b4962af0e7_iv -in encryptedfiles.tar.enc -out encryptedfiles.tar -d
+tar xvf encryptedfiles.tar
+rm encryptedfiles.tar
+
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_26b4962af0e7_key -iv $encrypted_26b4962af0e7_iv -in id-rsa.enc -out id-rsa -d
 mv id-rsa ~/.ssh/id_rsa
 rm id-rsa.enc
 chmod 600 ~/.ssh/id_rsa
 
 # Import Octopress 
-openssl aes-256-cbc -K $encrypted_26b4962af0e7_key -iv $encrypted_26b4962af0e7_iv -in Rakefile.enc -out Rakefile -d
 chmod 600 Rakefile
-rm Rakefile.enc
 
-openssl aes-256-cbc -K $encrypted_26b4962af0e7_key -iv $encrypted_26b4962af0e7_iv -in _config.yml.enc -out _config.yml -d
+
 chmod 600 _config.yml
-rm _config.yml.enc
+
 
 gem install bundler
 
