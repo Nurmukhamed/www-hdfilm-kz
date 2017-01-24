7a17c7647e1c79b10a00fe7eff76c5b2f9442680
Nurmukhamed Artykaly <nurmukh@nbc.kz>
2017-01-24 10:15:58 +0600

    Updated cert keys
    Updated cert files for encrypted files
    * Rakefile
    * _config.yml

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -8,11 +8,11 @@ rm id-rsa.enc
 chmod 600 ~/.ssh/id_rsa
 
 # Import Octopress 
-openssl aes-256-cbc -K $encrypted_26b4962af0e7_key -iv $encrypted_26b4962af0e7_iv -in Rakefile.enc -out Rakefile -d
+openssl aes-256-cbc -K encrypted_62f3167d5387_key -iv encrypted_62f3167d5387_iv -in Rakefile.enc -out Rakefile -d
 chmod 600 Rakefile
 rm Rakefile.enc
 
-openssl aes-256-cbc -K $encrypted_26b4962af0e7_key -iv $encrypted_26b4962af0e7_iv -in _config.yml.enc -out _config.yml -d
+openssl aes-256-cbc -K encrypted_62f3167d5387_key -iv encrypted_62f3167d5387_iv -in _config.yml.enc -out _config.yml -d
 chmod 600 _config.yml
 rm _config.yml.enc
 
