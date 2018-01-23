5f1688da18ce45a9221dcdcecd4219fadb52b642
Nurmukhamed Artykaly <nurmukh@nbc.kz>
2018-01-23 22:11:56 +0600

    gh-pages deploy

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     # Initialize a new git repo in _site, and push it to our server.
-    bundle exec rake deploy
+    echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+
+    # Go To Public folder
+    cd public
+    # Add changes to git.
+    git add .
+
+    # Commit changes.
+    msg="rebuilding site `date`"
+    if [ $# -eq 1 ]
+        then msg="$1"
+    fi
+    
+    git commit -m "$msg"
+
+    # Push source and build repos.
+    git push origin gh-pages
+
+    # Come Back up to the Project Root
+    cd ..
 else
     echo "Not deploying, since this branch isn't master."
 fi
