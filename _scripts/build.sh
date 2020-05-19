b4d28b00edfd025fd5ba0b69ddcc845aef94d1bb
Nurmukhamed Artykaly <87017586495@hdfilm.kz>
2020-05-19 11:48:10 +0600

    removed hugo-repository, fixing errors

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -2,7 +2,7 @@
 # build Octopress 
 rm -rf .git
 rm -rf public
-git clone git@github.com:Nurmukhamed/www-hdfilm-kz-hugo.git
+git clone git@github.com:Nurmukhamed/www-hdfilm-kz-octopress.git
 
 bundle exec rake generate
 
