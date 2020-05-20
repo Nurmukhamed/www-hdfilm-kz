36d60eeafd52bc2206f27f1f1e69dbdf6dc2a6ab
Nurmukhamed Artykaly <87017586495@hdfilm.kz>
2020-05-20 14:17:50 +0600

    troubleshooting

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 # build Octopress 
+HOMEDIR=/home/travis/build/Nurmukhamed
+REPODIR=/home/travis/build/Nurmukhamed/www-hdfilm-kz-octopress
+
+cd ${REPODIR}
+
+ls -l
+
 rm -rf public
 
 bundle exec rake generate
